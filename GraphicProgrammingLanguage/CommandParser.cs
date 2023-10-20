e85cf5f074debb86bfcb3fa26c3cb83401a497ef
iHarrrison <harrison.dickens0122@gmail.com>
2023-10-20 14:44:47 +0100

    Adds logic to command parser to split the command from the arguments

--- GraphicProgrammingLanguage/CommandParser.cs
+++ GraphicProgrammingLanguage/CommandParser.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GraphicProgrammingLanguage
 {
-    internal class CommandParser
+    public class CommandParser
     {
+        public string Command { get; private set; }
+        public string[] Args { get; private set; }
+
+        public CommandParser(string enteredCommand)
+        {
+            ParseCommand(enteredCommand);
+        }
+
+        private void ParseCommand(string commandText)
+        {
+            string[] commandParts = commandText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (commandParts.Length == 0)
+            {
+                Command = "";
+                Args = Array.Empty<string>();
+                return;
+            }
+
+            Command = commandParts[0];
+
+            if (commandParts.Length > 1)
+            {
+                Args = ParseArguments(commandParts, 1);
+            }
+            else
+            {
+                Args = Array.Empty<string>();
+            }
+        }
+
+        private string[] ParseArguments(string[] commandParts, int startIndex)
+        {
+            List<string> arguments = new List<string>();
+
+            for (int i = startIndex; i < commandParts.Length; i++)
+            {
+                arguments.Add(commandParts[i]);
+            }
+
+            return arguments.ToArray();
+        }
     }
 }
