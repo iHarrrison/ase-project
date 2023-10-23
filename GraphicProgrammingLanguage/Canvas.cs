7e63352178e27d5a1bc3f987caf84a7bd781fe9f
iHarrrison <harrison.dickens0122@gmail.com>
2023-10-23 12:14:47 +0100

    Adds the Rectangle functionality, manual tests show this is working as intended.

--- GraphicProgrammingLanguage/Canvas.cs
+++ GraphicProgrammingLanguage/Canvas.cs
@@ -15,6 +15,18 @@ namespace GraphicProgrammingLanguage
         {
             string enteredCommand = commandTextBox.Text;
             CommandParser parser = new CommandParser(pictureBox, enteredCommand);
+
+            // Switch statement to go through all expected commands and call the appropriate classes.
+            // TODO Add error handling so if it is not an expected command, it does not break.
+
+            switch (parser.Command.ToLower())
+            {
+                case "rectangle":
+                    RectangleCommand.Execute(pictureBox, parser.Args);
+                    break;
+                    // TODO add more commands.
+            }
         }
+
     }
 }
--- GraphicProgrammingLanguage/Rectangle.cs
+++ GraphicProgrammingLanguage/Rectangle.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace GraphicProgrammingLanguage
 {
-    internal class Rectangle
+    public class RectangleCommand
     {
+        public static void Execute(PictureBox pictureBox, string[] args)
+        {
+            if (args.Length >= 2)
+
+                // Look for string arguments for x and y values.
+            {
+                if (int.TryParse(args[0], out int width) && int.TryParse(args[1], out int height))
+                {
+                    int x = 0;
+                    int y = 0;
+
+                    if (pictureBox.Image == null)
+                    {
+                        // Initialize the PictureBox.Image if it's null, stopping errors.
+                        pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+                    }
+
+                    using (Graphics g = Graphics.FromImage(pictureBox.Image))
+                        // For now, just accepts black pen. This will be changed at a later date.
+                    {
+                        g.DrawRectangle(Pens.Black, x, y, width, height); 
+                    }
+
+                    pictureBox.Invalidate(); // Refreshes the PictureBox to see the changes.
+                }
+                else
+                {
+                    // Handles parsing errors via a windows message box.
+                    // Plan is to make this a separate class and just call a function as opposed to reusing code for
+                    // every shape...
+                    MessageBox.Show("Sorry, it appears you have entered invalid arguments for this command. " +
+                        "Please provide valid values to draw your shape.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
     }
 }
+
