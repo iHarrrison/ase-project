c92075fd6257f7d8337f7262cae47cbcd1aacd28
iHarrrison <harrison.dickens0122@gmail.com>
2023-10-23 13:08:49 +0100

    Creates the Drawing Position class with a view to adding methods in there to navigate the canvas. MoveTo is the only one in there so far, needs implementing in the canvas class.

---
+++ GraphicProgrammingLanguage/DrawingPosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicProgrammingLanguage
+{
+    public class DrawingPosition
+    {
+        public int x;
+        public int y;
+
+        // allows the initial drawing position to be set
+        public DrawingPosition(int initialX, int initialY)
+        {
+            x = initialX;
+            y = initialY;
+        }
+
+        // passes in new x and y coordinates to move the drawing position to a specified location.
+        public void MoveTo(int newX,  int newY)
+        {
+            x = newX;
+            y = newY;
+        }
+    }
+}
