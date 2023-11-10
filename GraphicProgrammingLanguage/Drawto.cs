d174eb4ded949ae5ecd3ef98fab22129f6df7e1c
iHarrrison <harrison.dickens0122@gmail.com>
2023-11-10 11:29:03 +0000

    XML Documentation for DrawTo

--- GraphicProgrammingLanguage/Drawto.cs
+++ GraphicProgrammingLanguage/Drawto.cs
@@ -1,10 +1,18 @@
-﻿// Drawto.cs
-using System;
+﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
 
+/// <summary>
+/// Enables the ability to draw lines from one place to another on the canvas
+/// </summary>
 public static class Drawto
 {
+    /// <summary>
+    /// Executes the drawto command, drawing a line from one coordinate to another
+    /// </summary>
+    /// <param name="pictureBox">The canvas in which the drawing occurs</param>
+    /// <param name="args">The array of strings containing the x and y coordinates</param>
+    /// <param name="drawingPosition">The current position for drawing, as well as settings (fill/pen color)</param>
     public static void Execute(PictureBox pictureBox, string[] args, DrawingPosition drawingPosition)
     {
         if (args.Length < 2)
