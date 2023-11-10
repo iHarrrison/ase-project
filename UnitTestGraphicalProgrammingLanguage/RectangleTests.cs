d952be823d7d97faf9f95e96357ac6a16f97785a
iHarrrison <harrison.dickens0122@gmail.com>
2023-11-10 12:40:33 +0000

    Implements Triangle and Rectangle Tests

--- UnitTestGraphicalProgrammingLanguage/RectangleTests.cs
+++ UnitTestGraphicalProgrammingLanguage/RectangleTests.cs
@@ -1,11 +1,40 @@
+using System.Windows.Forms;
+using GraphicProgrammingLanguage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace UnitTestGraphicalProgrammingLanguage
 {
     [TestClass]
     public class RectangleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Rectangle_Execute_Pass()
         {
+            // Given
+            var pictureBox = new PictureBox();
+            var args = new String[] { "20", "50" };
+            var drawingPosition = new DrawingPosition(250, 250);
+
+            // When
+            Rectangle.Execute(pictureBox, args, drawingPosition);
+
+            // Then
+            Assert.IsNotNull(pictureBox.Image);
+        }
+
+        [TestMethod]
+        public void Rectangle_Execute_Fail()
+        {
+            // Given
+            var pictureBox = new PictureBox();
+            var args = new String[] { "This is not a number!" };
+            var drawingPosition = new DrawingPosition(250, 250);
+
+            // When
+            Rectangle.Execute(pictureBox, args, drawingPosition);
+
+            // Then
+            Assert.IsNull(pictureBox.Image);
         }
     }
 }
--- UnitTestGraphicalProgrammingLanguage/TriangleTests.cs
+++ UnitTestGraphicalProgrammingLanguage/TriangleTests.cs
@@ -1,11 +1,40 @@
+using System.Windows.Forms;
+using GraphicProgrammingLanguage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace UnitTestGraphicalProgrammingLanguage
 {
     [TestClass]
     public class TriangleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Triangle_Execute_Pass()
         {
+            // Given
+            var pictureBox = new PictureBox();
+            var args = new String[] { "200" };
+            var drawingPosition = new DrawingPosition(250, 250);
+
+            // When
+            Triangle.Execute(pictureBox, args, drawingPosition);
+
+            // Then
+            Assert.IsNotNull(pictureBox.Image);
+        }
+
+        [TestMethod]
+        public void Triangle_Execute_Fail()
+        {
+            // Given
+            var pictureBox = new PictureBox();
+            var args = new String[] { "This is not a number!" };
+            var drawingPosition = new DrawingPosition(250, 250);
+
+            // When
+            Triangle.Execute(pictureBox, args, drawingPosition);
+
+            // Then
+            Assert.IsNull(pictureBox.Image);
         }
     }
 }
