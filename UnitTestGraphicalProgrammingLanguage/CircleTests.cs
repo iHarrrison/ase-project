306f44f8f329418807cfc81277232a70cb1d3199
iHarrrison <harrison.dickens0122@gmail.com>
2023-11-10 12:34:32 +0000

    Implements CircleTests

--- UnitTestGraphicalProgrammingLanguage/CircleTests.cs
+++ UnitTestGraphicalProgrammingLanguage/CircleTests.cs
@@ -1,11 +1,40 @@
-﻿namespace UnitTestGraphicalProgrammingLanguage
+﻿using System.Windows.Forms;
+using GraphicProgrammingLanguage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestGraphicalProgrammingLanguage
 {
     [TestClass]
     public class CircleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Circle_Execute_Pass()
         {
+            // Given
+            var pictureBox = new PictureBox();
+            var args = new String[] { "200" };
+            var drawingPosition = new DrawingPosition(250, 250);
+
+            // When
+            Circle.Execute(pictureBox, args, drawingPosition);
+
+            // Then
+            Assert.IsNotNull(pictureBox.Image);
+        }
+
+        [TestMethod]
+        public void Circle_Execute_Fail()
+        {
+            // Given
+            var pictureBox = new PictureBox();
+            var args = new String[] { "This is not a number!" };
+            var drawingPosition = new DrawingPosition(250, 250);
+
+            // When
+            Circle.Execute(pictureBox, args, drawingPosition);
+
+            // Then
+            Assert.IsNull(pictureBox.Image);
         }
     }
 }
