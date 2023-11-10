53a4d6d8f42bb43b8ca398bf3b1fbbc5f67c568e
iHarrrison <harrison.dickens0122@gmail.com>
2023-11-10 10:57:48 +0000

    XML documentation for Fill

--- GraphicProgrammingLanguage/Fill.cs
+++ GraphicProgrammingLanguage/Fill.cs
@@ -3,6 +3,11 @@ using System.Drawing;
 using System.Windows.Forms;
 public static class Fill
     {
+    /// <summary>
+    /// Sets the fill option to either true or not true for the drawing position
+    /// </summary>
+    /// <param name="drawingPosition">The current position for drawing, as well as settings (fill/pen color)</param>
+    /// <param name="args">An array of string arguments to set the fill to on or off</param>
     public static void Execute(DrawingPosition drawingPosition, string[] args)
     {
         if (args.Length <1)
@@ -26,5 +31,4 @@ public static class Fill
             MessageBox.Show("Invalid argument for Fill command (use 'on' or 'off').", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
-    
 }
