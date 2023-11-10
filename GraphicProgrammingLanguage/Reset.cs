dcb5fda8ee04d3e3f1c022b07435d41afaa4c252
iHarrrison <harrison.dickens0122@gmail.com>
2023-11-10 11:32:29 +0000

    XML Documentation for Reset

--- GraphicProgrammingLanguage/Reset.cs
+++ GraphicProgrammingLanguage/Reset.cs
@@ -2,8 +2,17 @@
 using System.Drawing;
 using System.Windows.Forms;
 
+/// <summary>
+/// Enables the ability to reset the drawing position
+/// </summary>
 public static class Reset
 {
+    /// <summary>
+    /// Executes the reset command, resetting the drawing position to 0, 0
+    /// </summary>
+    /// <param name="pictureBox">The canvas in which the drawing occurs</param>
+    /// <param name="args">The array of strings containing the width and height</param>
+    /// <param name="drawingPosition">The current position for drawing, as well as settings (fill/pen color)</param>
     public static void Execute(PictureBox pictureBox, string[] args, DrawingPosition drawingPosition)
     {
         // Reset the drawing position
