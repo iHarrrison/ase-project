3da4583fc1f493e57a5567652609c4f65adf5de3
iHarrrison <harrison.dickens0122@gmail.com>
2023-11-10 10:31:15 +0000

    Fixes incorrect unit test project, adds correct referencing.

---
+++ TestGraphicalProgrammingLanguage/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestGraphicalProgrammingLanguage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestGraphicalProgrammingLanguage")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82e9a728-50e8-4439-8485-0b6e8629b688")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- TestGraphicalProgrammingLanguage/UnitTest1.cs
+++ TestGraphicalProgrammingLanguage/UnitTest1.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
 namespace TestGraphicalProgrammingLanguage
 {
     [TestClass]
