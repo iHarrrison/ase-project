2a71a1f6cc5a46b20b6a5428841d29af5dc9e6c3
iHarrrison <harrison.dickens0122@gmail.com>
2023-11-10 11:07:47 +0000

    XML Documentation for CanvasPen

--- GraphicProgrammingLanguage/CanvasPen.cs
+++ GraphicProgrammingLanguage/CanvasPen.cs
@@ -2,8 +2,17 @@
 using System.Drawing;
 using System.Windows.Forms;
 
+/// <summary>
+/// Handles commands for the pen color for drawing on the canvas
+/// </summary>
 public static class CanvasPen
 {
+    /// <summary>
+    /// Executes the pen color command to select from specific colors for the canvas pen
+    /// </summary>
+    /// <param name="pictureBox">The canvas in which the drawing occurs</param>
+    /// <param name="args">The argument containing the pen color</param>
+    /// <param name="drawingPosition">The current position for drawing, as well as settings (fill/pen color)</param>
     public static void Execute(PictureBox pictureBox, string[] args, DrawingPosition drawingPosition)
     {
         if (args.Length < 1)
