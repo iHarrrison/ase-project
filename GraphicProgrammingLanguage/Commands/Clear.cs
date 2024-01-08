e3c4e7bb1b38de17320800a6879200264c72b717
iHarrrison <harrison.dickens0122@gmail.com>
2024-01-08 20:00:12 +0000

    Fix Clear bug

--- GraphicProgrammingLanguage/Commands/Clear.cs
+++ GraphicProgrammingLanguage/Commands/Clear.cs
@@ -9,6 +9,9 @@ public class Clear : AbstractGPLCommand
 {
     public override int ExpectedArgumentsCount => 0;
 
+    // Empty construct as Clear does not take args
+    public Clear(params object[] _) { }
+
     public override bool Execute(PictureBox pictureBox, DrawingPosition drawingPosition)
     {
         pictureBox.Image = new Bitmap(pictureBox.Size.Width, pictureBox.Size.Height);
